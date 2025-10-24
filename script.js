4e4c0054d736996ffd6822a754af26b16418fae1
rodriguez lagana jazmin <rodriguezlaganajazmin@gmail.com>
2025-10-24 00:55:05 -0300

    boton

--- script.js
+++
@@ -1,6 +0,0 @@
-const accordionBtn = document.querySelector('.accordion_btn');
-const accordionContent = document.querySelector('.accordion_content');
-
-accordionBtn.addEventListener('click', () => {
-  accordionContent.classList.toggle('show');
-});
